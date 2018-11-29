bc9cb09fb49eeaaead8c4ec63af3bb1a3a4ce5c5
Pavlo_Oliinyk1 <Pavlo_Oliinyk1@epam.com>
2018-11-29 18:47:07 +0200

    add : composition

--- Composition.js
+++ Composition.js
@@ -1,5 +1,6 @@
 class IComposer {
     select(id) {
+        console.log(this);
         throw new Error(`You must implement *select* method`);
     }
 
@@ -23,8 +24,8 @@ class Filter extends IComposer{
     }
 
     remove(child) {
-        var length = this.children.length;
-        for (var i = 0; i < length; i++) {
+        const length = this.children.length;
+        for (let i = 0; i < length; i++) {
             if (this.children[i] === child) {
                 this.children.splice(i, 1);
                 return;
@@ -40,31 +41,39 @@ class Filter extends IComposer{
         return this.children.length > 0;
     }
 
+    hasParent() {
+        return this.parentGroup;
+    }
+
     show(){
         return this.id
     }
 
     // recursively traverse a (sub)tree
-    select(node = this, indent = 1){
-        indent++;
-        for (var i = 0, len = node.children.length; i < len; i++) {
+    select(node = this){
+
+        if(!node.children.length) {
+            return `no chield found for ${node.id}, parentGroup is:${this.parentGroup.id}`;
+        }
+
+        for (let i = 0, len = node.children.length; i < len; i++) {
             this.selectedIds.push(node.getChild(i).id);
-            this.select(node.getChild(i), indent);
+            this.select(node.getChild(i));
             if(i === len-1) {
-                return this.selectedIds;
+                let arr = this.selectedIds;
+                this.selectedIds.unshift(this.parentGroup.id);
+                return (this.hasParent()) ? arr : this.selectedIds;
             }
         }
     }
 }
 
-
-
 const filters = new Filter("x Filters");
-const group1 = new Filter("-x Filter group 1");
+const group1 = new Filter("-x Filter group 1", filters);
 const group1_inner1 = new Filter("--x Filter 1", group1);
 const group1_inner2 = new Filter("--x Filter 2", group1);
 
-const group2 = new Filter("-x Filter group 2");
+const group2 = new Filter("-x Filter group 2", filters);
 const group2_inner1 = new Filter("--x FilterGr2 1", group2);
 const group2_inner2 = new Filter("--x FilterGr2 2", group2);
 
@@ -81,5 +90,6 @@ group2.add(group2_inner2);
 
 
 //test
-console.log(  group1_inner2.show() ); // => --x Filter 2
+console.log( group1_inner2.show()); // => --x Filter 2
 console.log(group2.select()); // => ["--x FilterGr2 1", "--x FilterGr2 2"]
+console.log(group1_inner2.select()); // => no chield found for--x Filter 2, parentGroup is:-x Filter group 1
